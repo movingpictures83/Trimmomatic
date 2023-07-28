825ce48ad1b06b839bbcb01b36a1b081fed87327
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2023-07-28 15:56:49 -0400

    Added single-endedt

--- TrimmomaticPlugin.cpp
+++ TrimmomaticPlugin.cpp
@@ -28,9 +28,10 @@ void TrimmomaticPlugin::run() {
 void TrimmomaticPlugin::output(std::string file) {
  // alpha_diversity.py -i filtered_otu_table.biom -m observed_species,chao1,shannon,PD_whole_tree -t rep_set.tre -o alpha.txt
    std::string command = "trimmomatic ";
+   if (arguments[0] == "PE") {
  for (int i = 0; i < arguments.size(); i++) {
 	 if (arguments[i].size() != 0) {
-		 if (hasEnding(arguments[i], "fastq")) {
+		 if (hasEnding(arguments[i], "fastq") || hasEnding(arguments[i], "fastq.gz")) {
                     arguments[i] = std::string(PluginManager::prefix()) + "/" + arguments[i];
 		 }
     command += arguments[i];
@@ -39,7 +40,17 @@ void TrimmomaticPlugin::output(std::string file) {
  }
  command += "-baseout "+file;
  std::cout << command << std::endl;
+   }
+ else {
+	 command += arguments[0] + " ";
+	 command +=  std::string(PluginManager::prefix()) + "/" + arguments[1] + " ";
+	command += file + " "; 
+ for (int i = 2; i < arguments.size(); i++) {
+    command += arguments[i];
+    command += " ";
+	 }
 
+ }
  system(command.c_str());
 }
 
